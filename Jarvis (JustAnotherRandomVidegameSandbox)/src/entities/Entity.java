5fd6e9e949901f8ee8fae4cf43870e8ef94a0b4b
OtherYellowGuy <alanliu2009@gmail.com>
2021-10-05 08:28:46 -0400

    Update Entity.java
    entity health and position variables

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/Entity.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/Entity.java
@@ -1,7 +1,10 @@
 package entities;
 
 import core.Coordinate;
+import gamestates.Game;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 public class Entity{
@@ -16,6 +19,9 @@ public class Entity{
 	
 	protected Coordinate position;
 	protected float xSpeed, ySpeed; // Entity velocity (pixels per second)
+	
+	protected float x, y, w, h;
+	protected int curHealth, maxHealth;
 	
 	/*
 	 * Render Variables
@@ -75,4 +81,24 @@ public class Entity{
 			ySpeed -= gravity;
 		}
 	}
+	
+	
+	//debug rendering
+	public void render(Graphics g) {
+		//write health of actor underneath
+		if (Game.debugMode) {
+			g.setColor(new Color(255, 255, 255));
+			g.drawString(""+(int) curHealth, x, y - 15);
+			g.setColor(new Color(0, 0, 0));
+		}
+		//debug for hitbox of actor
+		if (Game.debugMode) {
+			g.setColor(new Color(255, 255, 255));
+			g.drawRect(x, y, w, h);
+			g.setColor(new Color(0, 0, 0));
+		}
+	}
+	
+	
+	
 }
