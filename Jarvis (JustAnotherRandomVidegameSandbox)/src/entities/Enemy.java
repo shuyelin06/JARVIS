c98b2539cabb1d00304a0084fcb937f80745c1d9
kszed <82227471+kszed@users.noreply.github.com>
2021-10-05 08:48:29 -0400

    Create Enemy.java

---
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/Enemy.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Enemy extends Entity {
+	
+	protected int contactDmg;
+	
+	public Enemy() {
+		super(0,0); // implement stuff for deciding where to spawn
+		
+	}
+	
+	
+}
