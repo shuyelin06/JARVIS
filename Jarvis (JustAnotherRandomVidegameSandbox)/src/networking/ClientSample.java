5ddc1225f31df90f918ca55027134af6020db637
kszed <82227471+kszed@users.noreply.github.com>
2021-10-22 09:12:46 -0400

    enemy functionality

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/networking/ClientSample.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/networking/ClientSample.java
@@ -7,8 +7,8 @@ import java.io.*;
 public class ClientSample {
 
    public static void main(String [] args) {
-      String serverName = args[0];
-      int port = Integer.parseInt(args[1]);
+      String serverName = "localhost";
+      int port = Integer.parseInt("6066");
       try {
          System.out.println("Connecting to " + serverName + " on port " + port);
          Socket client = new Socket(serverName, port);
