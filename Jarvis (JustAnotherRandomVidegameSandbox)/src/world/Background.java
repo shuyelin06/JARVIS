e957cdc3a5898c726e4d9d44b2c730155f625271
SvenJing <stevenying2004@gmail.com>
2021-11-01 15:15:27 -0400

    y parallax

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/world/Background.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/world/Background.java
@@ -39,46 +39,48 @@ public class Background
 	{
 		sky.draw();
 		
+		y  *= 0.5f;
+		
 		//mountains
 		mountains.draw(x * 0.2f % Engine.RESOLUTION_X,
-				Engine.RESOLUTION_Y * 0.35f, 
+				Engine.RESOLUTION_Y * 0.35f + (y * 0.2f), 
 				Engine.RESOLUTION_X, 
 				Engine.RESOLUTION_Y * 0.5f);
 		
 		mountains.draw((x * 0.2f % Engine.RESOLUTION_X) + Engine.RESOLUTION_X, 
-				Engine.RESOLUTION_Y * 0.35f, 
+				Engine.RESOLUTION_Y * 0.35f + (y * 0.2f), 
 				Engine.RESOLUTION_X, 
 				Engine.RESOLUTION_Y * 0.5f);
 		
 		for(int i = 0; i < clouds.length; i++) //the clouds
 		{
-			clouds[i].render(g, (x  * 0.15f) % Engine.RESOLUTION_X, 0);
+			clouds[i].render(g, (x  * 0.15f) % Engine.RESOLUTION_X, y * 0.2f);
 		}
 		
 		//hills layer 1
 		hills.draw( (x * 0.25f % Engine.RESOLUTION_X) + Engine.RESOLUTION_X, //i should add a new texture
-				Engine.RESOLUTION_Y * 0.45f, 
+				Engine.RESOLUTION_Y * 0.45f + (y * 0.25f), 
 				-Engine.RESOLUTION_X, 
 				Engine.RESOLUTION_Y * 0.35f);
 		
 		hills.draw((x * 0.25f % Engine.RESOLUTION_X) + (2 * Engine.RESOLUTION_X),
-				Engine.RESOLUTION_Y * 0.45f,
+				Engine.RESOLUTION_Y * 0.45f + (y * 0.25f),
 				-Engine.RESOLUTION_X, 
 				Engine.RESOLUTION_Y * 0.35f);
 		
 		//hills layer 2
 		hills.draw(x * 0.3f % Engine.RESOLUTION_X, //first hill, loops back every time it travels its full length
-				Engine.RESOLUTION_Y * 0.5f, 
+				Engine.RESOLUTION_Y * 0.5f + (y * 0.3f), 
 				Engine.RESOLUTION_X, 
 				Engine.RESOLUTION_Y * 0.3f);
 		
 		hills.draw((x * 0.3f % Engine.RESOLUTION_X) + Engine.RESOLUTION_X, //second hill texture so it doesn't cut off
-				Engine.RESOLUTION_Y * 0.5f,
+				Engine.RESOLUTION_Y * 0.5f + (y * 0.3f),
 				Engine.RESOLUTION_X, 
 				Engine.RESOLUTION_Y * 0.3f);
 		
 		g.setColor(new Color(50, 122, 32));
-		g.fillRect(0, Engine.RESOLUTION_Y * 0.79f, Engine.RESOLUTION_X, Engine.RESOLUTION_Y * 0.5f);
+		g.fillRect(0, Engine.RESOLUTION_Y * 0.79f +  + (y * 0.3f), Engine.RESOLUTION_X, Engine.RESOLUTION_Y * 0.5f);
 
 	}
 }
