a05f64af498ab58f6eb65ea2ca2be908ec32b396
OtherYellowGuy <alanliu2009@gmail.com>
2021-11-03 08:22:53 -0400

    player has direction variable, no default direction

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/Entity.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/Entity.java
@@ -28,7 +28,7 @@ public class Entity{
 	
 	protected Coordinate position;
 	protected float xSpeed, ySpeed; // Entity velocity (pixels per second)
-	
+	protected boolean direction; //direction facing variable, 0 = left; 1 = right
 	
 	/*
 	 * Render Variables
@@ -59,6 +59,7 @@ public class Entity{
 		
 		this.xSpeed = 0f;
 		this.ySpeed = 0f;
+		this.direction = false;
 		
 		this.time = Sys.getTime();
 	}
@@ -130,7 +131,11 @@ public class Entity{
 		collisions();
 		
 		// Position updating
-		position.update(xSpeed, ySpeed);		
+		position.update(xSpeed, ySpeed);
+		
+		// Direction updating
+		if (xSpeed < 0) direction = false;
+		if (xSpeed > 0) direction = true;
 	}
 	
 	
@@ -278,6 +283,12 @@ public class Entity{
 		if(xSpeed < 0)
 		{
 			sprite.draw(x + sizeX * Coordinate.ConversionFactor, y, -sizeX * Coordinate.ConversionFactor, sizeY * Coordinate.ConversionFactor); 
+		} else if (xSpeed == 0) {
+			if (!direction) {
+				sprite.draw(x + sizeX * Coordinate.ConversionFactor, y, -sizeX * Coordinate.ConversionFactor, sizeY * Coordinate.ConversionFactor); 
+			} else {
+				sprite.draw(x, y, sizeX * Coordinate.ConversionFactor, sizeY * Coordinate.ConversionFactor); 
+			}
 		} else
 		{
 			sprite.draw(x, y, sizeX * Coordinate.ConversionFactor, sizeY * Coordinate.ConversionFactor); 
