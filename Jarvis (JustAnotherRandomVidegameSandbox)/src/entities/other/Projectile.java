ccbaccdc77e605ff23e343b1d770d68465489b0f
Pandakidz2 <shuyelin06@gmail.com>
2021-11-06 00:45:18 -0400

    ROCKEETS YEET

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/other/Projectile.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/other/Projectile.java
@@ -1,20 +1,52 @@
 package entities.other;
 
+import org.newdawn.slick.Image;
+
+import core.Engine;
 import entities.Entity;
 
 public class Projectile extends Entity{
 	
-	public Projectile(float x, float y){
+	private float speed = 50f;
+	
+	public Projectile(float x, float y, double theta){
 		super(x, y);
-	}
-	// Must also check Projectile - Player / Enemy Collisions
-	public void collisions() {
-		super.collisions();
 		
-//		if(Math.abs(p.getPosition().getX() - this.getPosition().getX()) <= 1) { //still need to implement hitboxes
-//			if(Math.abs(p.getPosition().getY() - this.getPosition().getY()) <= 1) {
-//				p.takeDamage(1, true);
-//			}
-//		}
+		this.sizeX = 0.5f;
+		this.sizeY = 0.5f;
+		
+		try {
+			sprite = new Image("res/placeholder.png");
+		} catch(Exception e) {}
+		
+		this.xSpeed = (float) Math.cos(theta) * speed;
+		this.ySpeed = (float) Math.sin(theta) * speed;
+	}
+
+	// Overwriting the update method
+	public void update() {
+		// Collision detection 
+		collisions();
+		
+		// Position updating
+		position.update(xSpeed, ySpeed);
+		
+		// Direction updating
+		if (xSpeed < 0) direction = false;
+		if (xSpeed > 0) direction = true;
+	}
+	protected void onCollision() {
+		int centerX = (int) position.getX();
+		int centerY = (int) position.getY();
+		
+		for(int i = -4; i < 5; i++)
+		{
+			for(int j = -4; j < 5; j++)
+			{
+				Engine.game.getWorld().destroyBlock(centerX + i, centerY + j);
+			}
+		}
+		
+		this.markForRemoval();
 	}
 }
