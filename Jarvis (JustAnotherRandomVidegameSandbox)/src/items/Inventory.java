e5ab11a4d8fa073f3c56bd756da4d5d433a76627
OtherYellowGuy <alanliu2009@gmail.com>
2021-11-29 08:51:15 -0500

    Update Inventory.java

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/items/Inventory.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/items/Inventory.java
@@ -83,6 +83,12 @@ public class Inventory{
 			Engine.game.addEntity(EntType.Items, eblock);
 		}	
 	}
-		
+	
+	public void swapElements(int index1, int index2) {
+		Item item1 = items[index1];
+		Item item2 = items[index2];
+		idIndexMapping.replace(item1.getID(), index2);
+		idIndexMapping.replace(item2.getID(), index1);
+	}
 
 }
