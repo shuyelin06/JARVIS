06b3f61712c96c1a5d2e998ae814326d2f6085c7
OtherYellowGuy <alanliu2009@gmail.com>
2021-12-15 09:08:11 -0500

    snowman carrot and title

--- Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/living/Snowman.java
+++ Jarvis (JustAnotherRandomVidegameSandbox)/src/entities/living/Snowman.java
@@ -1,6 +1,7 @@
 package entities.living;
 
 import core.Engine;
+import entities.projectiles.Carrot;
 import entities.projectiles.Snowball;
 import managers.ImageManager;
 import support.Utility;
@@ -8,6 +9,7 @@ import support.Utility;
 public class Snowman extends Enemy {
 	int shotCooldown;
 	int groupCooldown;
+	int carrotCooldown;
 	
 	public Snowman (float x, float y) {
 		super(x, y);
@@ -19,6 +21,7 @@ public class Snowman extends Enemy {
 		
 		shotCooldown = 0;
 		groupCooldown = 0;
+		carrotCooldown = 0;
 		contactDmg = 10;
 		aggroRange = 40;
 	
@@ -32,6 +35,7 @@ public class Snowman extends Enemy {
 		super.update();
 		shotCooldown++;
 		groupCooldown++;
+		carrotCooldown++;
 	}
 	
 	public boolean getPastDirection() {
@@ -47,13 +51,17 @@ public class Snowman extends Enemy {
 	public void ai (Player p) {
 		if(Utility.getDistance(this, p) <= aggroRange) {
 			
-			if (groupCooldown % 70 == 0) {
+			if (groupCooldown % 80 == 0) {
 				shotCooldown = 0;
 			}
+			
 			if (shotCooldown == 0 || shotCooldown == 4 || shotCooldown == 8) {
 				Engine.game.addProjectile(new Snowball(this, Engine.game.getPlayer().getPosition()));
 			}
 			
+			if (carrotCooldown % 200 == 0) {
+				Engine.game.addProjectile(new Carrot(this, Engine.game.getPlayer().getPosition()));
+			}
 			
 		}
 	}
